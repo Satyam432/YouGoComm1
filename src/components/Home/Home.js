cc28fbdfb2ae20fd01b11c6bd3467c5597c14bce
Satyam432 <kumarshatyam05@gmail.com>
2020-07-23 02:45:01 +0530

    home html

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,48 @@ import React from 'react';
 import './Home.css';
 
 const Home = () => {
-	return <div>Home</div>;
+	return <div>
+		<div className='LatestEvent w-75 p-3'>
+			<h3>
+				Event Name
+			</h3>
+			<p>
+				start (Date -time)__ end(Date -time)
+			</p>
+			<p>Details</p><br/>
+			<button className='RegisterButton btn btn-dark'>Register</button>
+		</div>
+		<div className='OngoingContest w-75 p-3'>
+			<h2>Ongoing Contest</h2>
+			<div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+				<ol className="carousel-indicators">
+					<li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+					<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+					<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+				</ol>
+				<div className="carousel-inner">
+					<div className="carousel-item active">
+						<img className="d-block w-100" src="..." alt="First slide" />
+					</div>
+					<div className="carousel-item">
+						<img className="d-block w-100" src="..." alt="Second slide" />
+					</div>
+					<div className="carousel-item">
+						<img className="d-block w-100" src="..." alt="Third slide" />
+					</div>
+				</div>
+				<a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+					<span className="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span className="sr-only">Previous</span>
+				</a>
+				<a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+					<span className="carousel-control-next-icon" aria-hidden="true"></span>
+					<span className="sr-only">Next</span>
+				</a>
+			</div>
+		</div><br/>
+
+	</div>;
 };
 
 export default Home;
